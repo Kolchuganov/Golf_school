19946e40c365c658bd7403fbb1b59c39a4029ded
Kolchuganov <d.kolchuganov@carx-tech.com>
2025-01-29 17:26:51 +0300

    Helper

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Golf
@@ -7,14 +8,18 @@ namespace Golf
     public class Player : MonoBehaviour
     {
         public Transform stick;
+        public Transform helper;
+
+        private Vector3 m_lastPosition;
         private bool m_isDown = false;
         public float range = 40f;
         public float speed = 500f;
+        public float power = 10f;
 
         private void Update()
         {
+            m_lastPosition = helper.position;
             m_isDown = Input.GetMouseButton(0);
-
             Quaternion rot = stick.localRotation;
             Quaternion toRot = Quaternion.Euler(0, 0, m_isDown ? range : -range);
             rot = Quaternion.RotateTowards(rot, toRot, speed * Time.deltaTime);
@@ -23,6 +28,13 @@ namespace Golf
 
         public void OnCollisionStick(Collider collider)
         {
+            if (collider.TryGetComponent(out Rigidbody stone))
+            {
+                //var dir = m_isDown ? stick.right : -stick.right;
+                var dir = (helper.position - m_lastPosition).normalized;
+                stone.AddForce(dir * power, ForceMode.Impulse);
+            }
+
             Debug.Log(collider, this);
         }
     }
