cc736f0d4b6d371d82e4e5d5613ad76da4df69a0
Kolchuganov <d.kolchuganov@carx-tech.com>
2025-01-30 12:30:16 +0300

    Player Controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,13 +19,17 @@ namespace Golf
         private void Update()
         {
             m_lastPosition = helper.position;
-            m_isDown = Input.GetMouseButton(0);
+           
             Quaternion rot = stick.localRotation;
             Quaternion toRot = Quaternion.Euler(0, 0, m_isDown ? range : -range);
             rot = Quaternion.RotateTowards(rot, toRot, speed * Time.deltaTime);
             stick.localRotation = rot;
         }
 
+        public void SetDown(bool value)
+        {
+            m_isDown = value;
+        }
 public void OnCollisionStick(Collider collider)
 {
     if (collider.TryGetComponent(out Rigidbody body))
